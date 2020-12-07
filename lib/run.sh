ea060d73cafd1e02c3d044737ba8ede8daf809b8
Mohamed Wajih Tagourty <wajih.tagourty@gmail.com>
2020-12-07 12:24:31 +0100

    added run script

---
+++ lib/run.sh
@@ -0,0 +1,1 @@
+cd 'd:\MyWorkSpace\project application mobil\foody'; & 'c:\Users\wajih\.vscode\extensions\vscjava.vscode-java-debug-0.29.0\scripts\launcher.bat' 'C:\Program Files\Java\jdk-11.0.9\bin\java.exe' '-Dfile.encoding=UTF-8' '@C:\Users\wajih\AppData\Local\Temp\cp_47ed9kn6linwqszbi0sk48k8l.argfile' 'com.imset.foody.FoodyApplication'
